e2db342794437b46d3a01a1799f950a78bd104ec
serenalotreck <serlot.grad@gmail.com>
2020-08-14 13:29:28 -0400

    First pass at cleaning up code

--- interpretation/swarmplots.py
+++ interpretation/swarmplots.py
@@ -28,12 +28,12 @@ def make_tidy_data(bin_df, features_scaled, y_name):
     returns:
     """
     # Get a list of the error bin of all instances
-    error_bin_IDs = bin_df.percent_error_bin_ID.copy()
+    error_bin_IDs = bin_df.abs_error_bin_ID.copy()
     error_bin_list = error_bin_IDs.unique()
 
     # Drop columns so that bin_df and features_scaled have same col names
-    bin_df = bin_df.drop(columns=[f'{y_name}_bin_ID', 'percent_error',
-                                'percent_error_bin_ID', y_name, 'bias', 'prediction'])
+    bin_df = bin_df.drop(columns=[f'{y_name}_bin_ID', 'abs_error',
+                                'abs_error_bin_ID', y_name, 'bias', 'prediction'])
     features_scaled = features_scaled.drop(columns=[y_name])
 
     # Stack bin_df
@@ -117,14 +117,13 @@ def make_bin_plot(bin_df, features_scaled, y_name, out_loc):
     plt.savefig(f'{out_loc}/{bin_ID[0]}_swarmplot.png')
 
 
-def make_swarmplots(interp_df, label_bin_df, out_loc, feature_values, y_name):
+def make_swarmplots(interp_df, out_loc, feature_values, y_name):
     """
     Makes six matplotlib figures, each with 6 subfigures. Each figure corresponds
     to one label bin, and each subplot is for an error bin within that label bin.
 
     parameters:
-        interp_df, pandas df: full dataframe from interp_file
-        label_bin_df, pandas df: interp_df with columns for bin IDs
+        interp_df, pandas df: interp_df with columns for bin IDs
         out_loc, str: path to save figures
         feature_values, pandas df: feature table
         y_name, str: Name of label column in feature_table
@@ -133,63 +132,37 @@ def make_swarmplots(interp_df, label_bin_df, out_loc, feature_values, y_name):
     features_scaled = feature_values.apply(lambda x: x/x.max())
 
     # Make plots
-    bins = label_bin_df[f'{y_name}_bin_ID'].unique()
+    bins = interp_df[f'{y_name}_bin_ID'].unique()
     for i, bin in enumerate(bins):
-        bin_df = label_bin_df[label_bin_df[f'{y_name}_bin_ID'] == bin].copy()
+        bin_df = interp_df[interp_df[f'{y_name}_bin_ID'] == bin].copy()
         bin_df_idx = bin_df.index.values.tolist()
         make_bin_plot(bin_df, features_scaled, y_name, out_loc)
 
-def get_bins(interp_df, value_name):
+
+def assign_error_bins(value_name, mean, std):
     """
-    Split instances into 6 bins based on the value of given column.
-
-    Bins:
-        | more than 2 SD below mean | between 1 and 2 SD below mean |
-        | between 1 and 0 SD below mean | between 0 and 1 SD above mean |
-        | between 1 and 2 SD above mean | more than 2 SD above mean |
-
-    parameters:
-        interp_df, pandas df: dataframe of interp_file or a subset of
-        value_name, str: name of the column to use
-
-    returns: interp_df with a column for the bin ID of each instance
+    Function to assign error bin ID absed on absolute error
     """
-    # Get label column
-    values = interp_df[value_name]
+    if value_name <= mean-0.5*std:
+        return f'abs_error_bin0'
+    elif mean-0.5*std < value_name <= mean+0.5*std:
+        return f'abs_error_bin1'
+    elif mean+0.5*std < value_name:
+        return f'abs_error_bin2'
 
-    # Calcualte mean and SD
-    mean = values.mean()
-    SD = values.std()
 
-    # Get min and max label values
-    min = values.min()
-    max = values.max()
-
-    # Get bin bounds
-    bin0 = mean-2*SD
-    bin1 = (mean-2*SD, mean-SD)
-    bin2 = (mean-SD, mean)
-    bin3 = (mean, mean+SD)
-    bin4 = (mean+SD, mean+2*SD)
-    bin5 = mean+2*SD
-    bin_list = [bin0, bin1, bin2, bin3, bin4, bin5]
-
-    # Get bin ID for each instance
-    bin_col_dfs = []
-    for i, bin in enumerate(bin_list):
-        if i == 0:
-            bin_col_df = interp_df[bin > interp_df[value_name]].copy()
-        elif i == 5:
-            bin_col_df = interp_df[bin < interp_df[value_name]].copy()
-        else:
-            bin_col_df = interp_df[(bin[0] < interp_df[value_name]) &
-                                (interp_df[value_name] < bin[1])].copy()
-        bin_col_df[f'{value_name}_bin_ID'] = f'{value_name}_bin{i}'
-        bin_col_dfs.append(bin_col_df)
-
-    interp_df_binned = pd.concat(bin_col_dfs)
-
-    return interp_df_binned
+def assign_quartile_bins(value_name, q1, q2, q3, y_name):
+    """
+    Function to assign Y binID based on value in value_name column
+    """
+    if value_name <= q1:
+        return f'{y_name}_q1'
+    elif q1 < value_name <= q2:
+        return f'{y_name}_q2'
+    elif q2 < value_name <= q3:
+        return f'{y_name}_q3'
+    elif value_name > q3:
+        return f'{y_name}_q4'
 
 
 def get_top_ten(imp, interp_df, feature_values):
@@ -211,6 +184,7 @@ def get_top_ten(imp, interp_df, feature_values):
         feature_values, pandas df: feature matrix with only top ten features,
             renamed feature1 to feature10
     """
+    # Get top ten and drop others
     if len(imp.mean_imp) > 10:
         top_ten = imp.index.tolist()[:10]
         others = imp.index.tolist()[10:]
@@ -257,58 +231,46 @@ def get_top_ten(imp, interp_df, feature_values):
     return interp_df, feature_values
 
 
-def main(interp_file, feature_table, imp_file, sep_interp, sep_feat,
-        y_name, feature_selection, out_loc):
+def main(interp_df, feature_values, imp, y_name, out_loc):
     """
     Generates swarmplots.
 
     parameters:
-        interp_file, str: path to the file containing treeinterpreter output
-            from the ML pipeline
-        feature_table, str: path to the file containing the feature table
-        imp_file, str: path to the file containing gini importances from the
-            ML pipeline
-        sep_interp, str: delimiter for interp_file
-        sep_feat, str: delimiter for the feature table
+        interp_df, str: treeinterpreter output from the ML pipeline
+        feature_values, df: feature table used to train the model
+        imp, df: gini importances from the ML pipeline
         y_name, str: Name of label column in feature_table
-        feature_selection, str: filename with list of features used in model
         out_loc, str: path to save plots
     """
-    # Read in the data
-    imp = pd.read_csv(imp_file, index_col=0, sep='\t', engine='python')
-    interp_df = pd.read_csv(interp_file, index_col=0, sep=sep_interp, engine='python')
-    feature_values = pd.read_csv(feature_table, index_col=0, sep=sep_feat, engine='python')
-    print(f'first five column names: {feature_values.columns.values.tolist()[:5]}')
-    if os.path.isfile(feature_selection):
-        with open(feature_selection) as f:
-            features = f.read().strip().splitlines()
-            features = [y_name] + features
-            print(f'first five of features list: {features[:5]}')
-            feature_values = feature_values[features]
-
     # Get the ten features to use in plots
     print('==> Getting top ten most important features <==')
     interp_df, feature_values = get_top_ten(imp, interp_df, feature_values)
 
     # Get distribution of the label and put in bins
     print('\n\n==> Separating instances into bins based on label <==')
-    label_bin_df = get_bins(interp_df, y_name)
-    print(f'\nSnapshot of dataframe with label bin column added:\n\n {label_bin_df.head()}')
+    percentiles = np.percentile(interp_df[y_name], [25, 50, 75])
+    interp_df['label_bin'] = interp_df.apply(lambda x: my_func(x['y_name'],
+                                            percentiles[0], percentiles[1],
+                                            percentiles[2], y_name), axis=1)
+    print(f'\nSnapshot of dataframe with label bin column added:\n\n {_df.head()}')
 
     # get error for all instances
     print('\n\n==> Calculating error for all instances <==')
-    label_bin_df['percent_error'] = mp.calculate_error(label_bin_df['prediction'],
-                                                        label_bin_df[y_name])
-    print(f'\nSnapshot of dataframe with error column added:\n\n {label_bin_df.head()}')
+    interp_df['abs_error'] = interp_df[y_name] - interp_df['prediction']
+
+    print(f'\nSnapshot of dataframe with error column added:\n\n {interp_df.head()}')
 
     # Split each label bin into error bins
     print('\n\n==> Separating instances into bins by error <==')
-    label_bin_df = get_bins(label_bin_df,'percent_error')
-    print(f'\nSnapshot of dataframe with error bin column added:\n\n {label_bin_df.head()}')
+    interp_df['abs_error_bin'] = interp_df.apply(lambda x: assign_error_bins(
+                                                            x['abs_error'],
+                                                            x['abs_error'].mean(),
+                                                            x['abs_error'].std()))
+    print(f'\nSnapshot of dataframe with error bin column added:\n\n {interp_df.head()}')
 
     # Make swarmplots
     print('\n\n==> Making swarmplots <==')
-    make_swarmplots(interp_df, label_bin_df, out_loc,
+    make_swarmplots(interp_df, out_loc,
                    feature_values, y_name)
     print('\nSwarmplots finished!')
 
@@ -335,12 +297,25 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    # Get absolute paths
     args.interp_file = os.path.abspath(args.interp_file)
     args.feature_table = os.path.abspath(args.feature_table)
     args.imp_file = os.path.abspath(args.imp_file)
     args.feature_selection = os.path.abspath(args.feature_selection)
     args.out_loc = os.path.abspath(args.out_loc)
 
+    # Read in the data
+    imp = pd.read_csv(args.imp_file, index_col=0, sep='\t', engine='python')
+    interp_df = pd.read_csv(args.interp_file, index_col=0, sep=args.sep_interp, engine='python')
+    feature_values = pd.read_csv(args.feature_table, index_col=0, sep=args.sep_feat, engine='python')
+    print(f'first five column names: {feature_values.columns.values.tolist()[:5]}')
 
-    main(args.interp_file, args.feature_table, args.imp_file, args.sep_interp,
-    args.sep_feat, args.y_name, args.feature_selection, args.out_loc)
+    # Subset selected features if applicable
+    if os.path.isfile(args.feature_selection):
+        with open(args.feature_selection) as f:
+            features = f.read().strip().splitlines()
+            features = [y_name] + features
+            print(f'first five of features list: {features[:5]}')
+            feature_values = feature_values[features]  # Chooses only the features used to train the model
+
+    main(interp_df, feature_values, imp, args.y_name, args.out_loc)
